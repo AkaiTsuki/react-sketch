a6fbde0697fd296425e99ec9d4d15313e0d95f4e
Jiachi Liu <liu.jiach@husky.neu.edu>
2016-02-13 23:25:40 -0500

    New Feature
    1 unselect all widget when click on empty space in canvas
    2 add support to align top and bottom

--- app/components/console/FunctionalConsole.jsx
+++ app/components/console/FunctionalConsole.jsx
@@ -1,17 +1,20 @@
 import React, { Component, PropTypes } from 'react';
+import * as CanvasActionType from '../../constants/CanvasActionType';
 
 export default class FunctionalConsole extends Component {
   constructor(props, context) {
     super(props, context);
     this._onAlighLeftClick = this._onAlighLeftClick.bind(this);
     this._onAlighRightClick = this._onAlighRightClick.bind(this);
+    this._onAlighTopClick = this._onAlighTopClick.bind(this);
     this._onDeleteClick = this._onDeleteClick.bind(this);
+    this._onAlighBottomClick = this._onAlighBottomClick.bind(this);
   }
 
   render(){
 
     const btnStyle = {
-      marginRight: '5px'
+      margin: '0 5px 5px 0'
     }
 
     return(
@@ -27,6 +30,14 @@ export default class FunctionalConsole extends Component {
           <i className="fa fa-align-right pull-left"></i>
           Align Right
         </a>
+        <a className="btn btn-default" style={btnStyle} onClick={this._onAlighTopClick}>
+          <i className="fa fa-align-top pull-left"></i>
+          Align Top
+        </a>
+        <a className="btn btn-default" style={btnStyle} onClick={this._onAlighBottomClick}>
+          <i className="fa fa-align-bottom pull-left"></i>
+          Align Bottom
+        </a>
         <a className="btn btn-default" style={btnStyle} onClick={this._onDeleteClick}>
           <i className="fa fa-trash pull-left"></i>
           Remove Widgets
@@ -38,13 +49,25 @@ export default class FunctionalConsole extends Component {
   _onAlighLeftClick(e){
     const {actions, selected} = this.props;
     const selectWidgetIds = this.getSelectWidgetIds(selected);
-    if(selectWidgetIds.length > 1) actions.alignWidgets(selectWidgetIds, "left");
+    if(selectWidgetIds.length > 1) actions.alignWidgets(selectWidgetIds, CanvasActionType.ALIGN_DIR_LEFT);
   }
 
   _onAlighRightClick(e){
     const {actions, selected} = this.props;
     const selectWidgetIds = this.getSelectWidgetIds(selected);
-    if(selectWidgetIds.length > 1) actions.alignWidgets(selectWidgetIds, "right");
+    if(selectWidgetIds.length > 1) actions.alignWidgets(selectWidgetIds, CanvasActionType.ALIGN_DIR_RIGHT);
+  }
+
+  _onAlighTopClick(e){
+    const {actions, selected} = this.props;
+    const selectWidgetIds = this.getSelectWidgetIds(selected);
+    if(selectWidgetIds.length > 1) actions.alignWidgets(selectWidgetIds, CanvasActionType.ALIGN_DIR_TOP);
+  }
+
+  _onAlighBottomClick(e){
+    const {actions, selected} = this.props;
+    const selectWidgetIds = this.getSelectWidgetIds(selected);
+    if(selectWidgetIds.length > 1) actions.alignWidgets(selectWidgetIds, CanvasActionType.ALIGN_DIR_BOTTOM);
   }
 
   _onDeleteClick(e){
--- app/support/AlignmentSupport.js
+++ app/support/AlignmentSupport.js
@@ -1,3 +1,5 @@
+import * as CanvasActionType from '../constants/CanvasActionType';
+
 const alignLeft = (widgets, ids) => {
   const xPositions = ids.map(id => widgets[id].x);
   const alignedXPosition = xPositions.reduce((prev, curr) => prev < curr ? prev : curr );
@@ -20,7 +22,35 @@ const alignRight = (widgets, ids) => {
 
 const applyXPositionToSelectedWidgetsForRightAlign = (widgets, ids, alignedXPosition) => {
   ids.forEach(id => {
-    widgets[id].x = alignedXPosition -   widgets[id].width;
+    widgets[id].x = alignedXPosition -  widgets[id].width;
+  });
+
+  return widgets;
+}
+
+const alignTop = (widgets, ids) => {
+  const topPositions = ids.map(id => widgets[id].y);
+  const alignedTopPosition = topPositions.reduce((prev, curr) => prev < curr ? prev : curr);
+  return applyYPositionToSelectedWidgetsForTopAlign(widgets, ids, alignedTopPosition);
+}
+
+const applyYPositionToSelectedWidgetsForTopAlign = (widgets, ids, position) => {
+  ids.forEach(id => {
+    widgets[id].y = position;
+  });
+
+  return widgets;
+}
+
+const alignBottom = (widgets, ids) => {
+  const bottomPositions = ids.map(id => widgets[id].y + widgets[id].height);
+  const position = bottomPositions.reduce((prev, curr) => prev > curr ? prev : curr );
+  return applyYPositionToSelectedWidgetsForBottomAlign(widgets, ids, position);
+}
+
+const applyYPositionToSelectedWidgetsForBottomAlign = (widgets, ids, position) => {
+  ids.forEach(id => {
+    widgets[id].y = position - widgets[id].height;
   });
 
   return widgets;
@@ -28,10 +58,14 @@ const applyXPositionToSelectedWidgetsForRightAlign = (widgets, ids, alignedXPosi
 
 export const alignWidgets = (widgets, selectedWidgetIds, direction) => {
   switch(direction){
-    case "left":
+    case CanvasActionType.ALIGN_DIR_LEFT:
       return alignLeft(widgets, selectedWidgetIds);
-    case "right":
+    case CanvasActionType.ALIGN_DIR_RIGHT:
       return alignRight(widgets, selectedWidgetIds);
+    case CanvasActionType.ALIGN_DIR_TOP:
+      return alignTop(widgets, selectedWidgetIds);
+    case CanvasActionType.ALIGN_DIR_BOTTOM:
+      return alignBottom(widgets, selectedWidgetIds);
     default:
       console.error("Unsupported alignment direction: "+ direction);
   }
